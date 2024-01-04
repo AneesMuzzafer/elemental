2cfdd5c52b4ff3584e874037f077e94c7117879c
Anees Muzzafer <aneesmuzzafer@gmail.com>
2024-01-04 22:37:46 +0530

    Revert "✨ Make Home, Register, Login pages"
    This reverts commit 5c6ce06d0897409478f119e896e286d5c7d764b9.

--- app/routes.php
+++ app/routes.php
@@ -6,11 +6,3 @@ use Core\Facade\Route;
 Route::get("/", function () {
     return view("Welcome")->withLayout("layouts.DashboardLayout");
 });
-
-Route::get("/register", function () {
-    return view("Register")->withLayout("layouts.DashboardLayout");
-});
-
-Route::get("/login", function () {
-    return view("Login")->withLayout("layouts.DashboardLayout");
-});
