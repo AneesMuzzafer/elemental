6c6ce06d0897409478f119e896e286d5c7d764b9
Anees Muzzafer <aneesmuzzafer@gmail.com>
2024-01-04 20:41:58 +0530

    ✨ Make Home, Register, Login pages

--- app/routes.php
+++ app/routes.php
@@ -6,3 +6,11 @@ use Core\Facade\Route;
 Route::get("/", function () {
     return view("Welcome")->withLayout("layouts.DashboardLayout");
 });
+
+Route::get("/register", function () {
+    return view("Register")->withLayout("layouts.DashboardLayout");
+});
+
+Route::get("/login", function () {
+    return view("Login")->withLayout("layouts.DashboardLayout");
+});
