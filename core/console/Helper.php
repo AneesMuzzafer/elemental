bfac228ec77e81972deb7336c50803dcba2cc957
Afaan Bilal <afaanbilal@gmail.com>
2024-01-19 21:02:28 +0530

    📝 Route list

--- core/console/Helper.php
+++ core/console/Helper.php
@@ -55,4 +55,9 @@ class Helper
     {
         return "\033[1;31m" . $message . "\033[0m";
     }
+
+    public static function blueText(string $message): string
+    {
+        return "\033[1;34m" . $message . "\033[0m";
+    }
 }
